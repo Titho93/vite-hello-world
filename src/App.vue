c8e37a0e90e818593d30d22805692b3ddaefe6f8
titho93 <titho93.10@gmail.com>
2023-10-09 15:14:07 +0200

    [add]bonus Jumbotron

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <script>
+import Jumbotron from './components/Jumbotron.vue';
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Jumbotron,
+  }
 }
 </script>
 
 <template>
 
   <h1>La mia prima app con Vite!</h1>
+  <Jumbotron />
   
 </template>
 <style>
---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    title: 'Jumbotron'
+}
+</script>
+<template>
+
+    <h2>Questo è il Jumbotron</h2>
+  
+</template>
+<style>
+
+</style>
